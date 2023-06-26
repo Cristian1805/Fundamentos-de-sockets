851c13e31cf7f725c22b9bdda3586e9d01d51564
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-06-26 17:44:06 -0500

    Retroalimentación de emisiones del cliente hacia el servidor

--- models/server.js
+++ models/server.js
@@ -43,11 +43,16 @@ class Server {
             console.log('Cliente conectado', socket.id );
 
             socket.on('disconnect', () =>{
-                console.log('Cliente desconectado', socket.id );
+                
+                // console.log('Cliente desconectado', socket.id );
             });
 
-            socket.on('enviar-mensaje', (payload) => {
-                console.log(payload)
+            socket.on('enviar-mensaje', (payload, callback) => {
+
+                const id = 123456;
+                callback (id);
+
+                // this.io.emit('enviar-mensaje', payload);
             });
         });
     }
